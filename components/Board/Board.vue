97dedfc7921a73e08050da4e2cf558633ed6e07f
Vítor Barroso <contact@owozsh.dev>
2023-07-11 23:35:20 -0300

    make dnd work

--- components/Board/Board.vue
+++ components/Board/Board.vue
@@ -1,38 +1,72 @@
+<script setup lang="ts">
+import { BoardRes } from "~/lib/store/board";
+import { useUserStore } from "~/lib/store/user";
+
+const store = useBoardStore();
+
+store.get_board();
+</script>
+
 <script lang="ts">
 import { Container, Draggable } from "vue3-smooth-dnd";
-import { getBoard } from "~/lib/services/boards";
 import { useBoardStore } from "~/lib/store/board";
-
+import { Board } from "~/lib/types/board";
 export default {
   name: "app",
   components: { Container, Draggable },
-  data() {
-    const store = useBoardStore();
-    store.get_board();
-
-    return {
-      board: store.board,
-    };
-  },
+  data() {},
   methods: {
     onDrop(dropResult: any) {
-      console.log(dropResult);
+      const store = useBoardStore();
 
-      this.board.columns = this.applyDrag(this.board?.columns, dropResult);
+      store.update_board({
+        ...store.$state.board,
+        columns: this.applyDrag(store.$state.board.columns, dropResult),
+      });
     },
-    applyDrag(arr: any, dragResult: any) {
-      const { removedIndex, addedIndex, payload } = dragResult;
+    onCardDrop(columnId: string, dropResult: any) {
+      const store = useBoardStore();
+
+      if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
+        const scene = Object.assign({}, store.$state.board);
+        const column = scene.columns.filter((p) => p.id === columnId)[0];
+        const columnIndex = scene.columns.indexOf(column);
+
+        const newColumn = Object.assign({}, column);
+        newColumn.cards = this.applyDrag(newColumn.cards, dropResult);
+        scene.columns.splice(columnIndex, 1, newColumn);
+
+        store.update_board(scene);
+      }
+    },
+
+    applyDrag(
+      arr: any,
+      dropResult: {
+        removedIndex: number | null;
+        addedIndex: number | null;
+        payload?: {
+          columnId: string;
+          cardId: string;
+          cardIndex: number;
+        };
+      }
+    ) {
+      const { addedIndex, removedIndex, payload } = dropResult;
 
       if (removedIndex === null && addedIndex === null) return arr;
+
       const result = [...arr];
       let itemToAdd = payload;
 
       if (removedIndex !== null) {
         itemToAdd = result.splice(removedIndex, 1)[0];
       }
+
       if (addedIndex !== null) {
         result.splice(addedIndex, 0, itemToAdd);
       }
+
       return result;
     },
   },
@@ -43,21 +77,36 @@ export default {
   <div class="flex">
     <Container @drop="onDrop" orientation="horizontal" group-name="columns">
       <Draggable
-        v-for="(column, i) in board.columns"
+        v-for="(column, i) in store.$state.board.columns"
         :key="column.id"
         group-name="columns"
       >
         <div
-          class="group/column bg-base-100 mr-4 pt-4 rounded-lg outline-1 outline outline-base-50 -outline-offset-2 shadow-sm border border-solid border-base-300 cursor-pointer active:cursor-grabbing"
+          class="w-72 group/column bg-base-100 mr-4 pt-4 rounded-lg outline-1 outline outline-base-50 -outline-offset-2 shadow-sm border border-solid border-base-300 cursor-pointer active:cursor-grabbing"
         >
-          <input
-            class="bg-transparent text-sm ml-3 mb-3 px-1 text-base-800"
-            :modelValue="column.title"
-            @update:modelValue="(newValue: string) => (column.title = newValue)"
-            @mousedown.stop
-            @keydown.esc="($event.target as HTMLInputElement).blur()"
-          />
-          <Container @drop="onDrop" group-name="cards">
+          <div class="flex items-center justify-between px-4 mb-2">
+            <input
+              class="bg-transparent text-sm px-1 text-base-800"
+              :value="column.title"
+              @input="
+                (e) => store.update_column_title(column.id, e.target.value)
+              "
+              @mousedown.stop
+              @keydown.esc="($event.target as HTMLInputElement).blur()"
+            />
+            <button
+              class="cursor-default rounded-md hover:bg-base-200 p-2"
+              v-on:click="() => store.delete_column(column.id)"
+            >
+              <span class="i-mdi-trash-can"></span>
+            </button>
+          </div>
+
+          <Container
+            @drop="(e) => onCardDrop(column.id, e)"
+            group-name="cards"
+            :get-child-payload="(e) => store.$state.board.columns[i].cards[e]"
+          >
             <Draggable
               v-for="(card, i) in column.cards"
               :key="card.id"
@@ -65,17 +114,29 @@ export default {
               class="p-2 pt-0"
             >
               <div
-                class="group flex items-center justify-between text-base-800 text-sm shadow-sm cursor-pointer active:cursor-grabbing w-60 rounded-lg bg-base-50 border border-solid border-base-300 p-4 mb-1 max-h-12"
+                class="w-full group flex items-center justify-between text-base-800 text-sm shadow-sm cursor-pointer active:cursor-grabbing rounded-lg bg-base-50 border border-solid border-base-300 p-4 mb-1 max-h-12"
               >
-                <p class="truncate ... overflow-hidden">
-                  {{ card.title }}
-                </p>
+                <input
+                  class="bg-transparent text-sm px-1 text-base-800 truncate ... overflow-hidden w-full"
+                  :value="card.title"
+                  @input="
+                    (e) =>
+                      store.update_card_title(
+                        column.id,
+                        card.id,
+                        e.target.value
+                      )
+                  "
+                  @mousedown.stop
+                  @keydown.esc="($event.target as HTMLInputElement).blur()"
+                />
               </div>
             </Draggable>
           </Container>
           <div class="hidden group-hover/column:flex p-2 pt-0">
             <button
-              class="text-base-600 flex items-center transition-all pl-4 text-sm cursor-pointer w-60 rounded-lg hover:bg-base-200 border border-solid border-transparent hover:border-base-300 p-2 mb-1"
+              v-on:click="() => store.add_card(column.id)"
+              class="w-full cursor-default text-base-600 flex items-center transition-all pl-4 text-sm rounded-lg hover:bg-base-200 border border-solid border-transparent hover:border-base-300 p-2 mb-1"
             >
               <span class="i-mdi-plus mr-3"> </span>
               Add Card
@@ -85,10 +146,8 @@ export default {
       </Draggable>
     </Container>
     <button
-      :on-click="
-        () => board.columns.push({ id: '1', title: 'New Column', cards: [] })
-      "
-      class="text-base-600 h-12 flex items-center pl-4 transition-all text-sm cursor-pointer w-60 rounded-lg p-2 mb-1 hover:bg-base-200 border border-solid border-transparent hover:border-base-300"
+      v-on:click="store.add_column"
+      class="cursor-default text-base-600 h-12 flex items-center pl-4 transition-all text-sm w-60 rounded-lg p-2 mb-1 hover:bg-base-200 border border-solid border-transparent hover:border-base-300"
     >
       <span class="i-mdi-plus mr-3"> </span>
       Add Column
--- lib/store/board.ts
+++ lib/store/board.ts
@@ -1,17 +1,20 @@
 import { defineStore } from "pinia";
 import { Board, Column } from "../types/board";
 import { useUserStore } from "./user";
+import { v4 } from "uuid";
 
 const base_endpoint = "board";
 
-type BoardRes = {
+export type BoardRes = {
   id: string;
   createdAt: string;
   updatedAt: string;
   title: string;
   backgroundColor: string;
   textColor: string;
-  content: Column[];
+  content: {
+    columns: Column[];
+  };
 };
 
 export const useBoardStore = defineStore("board", {
@@ -23,7 +26,6 @@ export const useBoardStore = defineStore("board", {
       columns: [],
     } as Board,
   }),
-
   actions: {
     async get_board() {
       const config = useRuntimeConfig();
@@ -43,13 +45,15 @@ export const useBoardStore = defineStore("board", {
 
       this.board = {
         title: res.title,
-        columns: res.content,
+        columns: res.content.columns,
         id: res.id,
         metadata: {
           backgroundColor: res.backgroundColor,
           textColor: res.textColor,
         },
       };
+
+      return this.board;
     },
     async update_board(board: Board) {
       const config = useRuntimeConfig();
@@ -58,13 +62,100 @@ export const useBoardStore = defineStore("board", {
 
       this.board = board;
 
-      await $fetch(`${base_endpoint}/${route.params.board}`, {
-        method: "POST",
-        body: JSON.stringify(board),
+      await $fetch(`${base_endpoint}/${route.params.board}/update`, {
+        method: "PUT",
+        body: JSON.stringify({
+          title: board.title,
+          backgroundColor: board.metadata.backgroundColor,
+          content: {
+            columns: board.columns,
+          },
+          createdAt: board.metadata.createdAt,
+          updatedAt: new Date().toISOString(),
+        } as BoardRes),
         baseURL: config.public.baseURL,
         headers: {
           authorization: auth_token || "",
         },
+      });
+    },
+    async add_column() {
+      await this.update_board({
+        ...this.board,
+        columns: [
+          ...this.board.columns,
+          {
+            id: v4(),
+            title: "",
+            cards: [],
+          },
+        ],
+      });
+    },
+    async update_column_title(column_id: string, title: string) {
+      await this.update_board({
+        ...this.board,
+        columns: this.board.columns.map((column) => {
+          if (column.id === column_id) {
+            return {
+              ...column,
+              title,
+            };
+          }
+
+          return column;
+        }),
+      });
+    },
+    async delete_column(column_id: string) {
+      await this.update_board({
+        ...this.board,
+        columns: this.board.columns.filter((column) => column.id !== column_id),
+      });
+    },
+    async add_card(column_id: string) {
+      await this.update_board({
+        ...this.board,
+        columns: this.board.columns.map((column) => {
+          if (column.id === column_id) {
+            return {
+              ...column,
+              cards: [
+                ...column.cards,
+                {
+                  id: v4(),
+                  title: "",
+                },
+              ],
+            };
+          }
+
+          return column;
+        }),
+      });
+    },
+    async update_card_title(column_id: string, card_id: string, title: string) {
+      await this.update_board({
+        ...this.board,
+        columns: this.board.columns.map((column) => {
+          if (column.id === column_id) {
+            return {
+              ...column,
+              cards: column.cards.map((card) => {
+                if (card.id === card_id) {
+                  return {
+                    ...card,
+                    title,
+                  };
+                }
+
+                return card;
+              }),
+            };
+          }
+
+          return column;
+        }),
       });
     },
   },
